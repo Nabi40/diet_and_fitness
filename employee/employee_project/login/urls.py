bf71a664a758abf1df6065bf0777dd0ff87b2638
Nabi40 <132945433+Nabi40@users.noreply.github.com>
2023-11-19 01:01:15 +0600

    Delete employee/employee_project directory

--- employee/employee_project/login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path 
-from . import views
-
-urlpatterns = [
-    path('', views.index, name ='root'),
-    path('auth/signup', views.signup, name ='signup'),
-    path('auth/login', views.login_view, name ='login'),
-    path('auth/logout', views.logout_view, name ='logout'),
-]
